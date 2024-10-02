698fc543ae1105efc1cbd6fa40957cbe237789cb
Mariano Tourné <marianotourne@gmail.com>
2024-10-02 08:30:06 -0300

    working

--- src/Advice/Advice.jsx
+++ src/Advice/Advice.jsx
@@ -1,27 +1,41 @@
-/* eslint-disable react/prop-types */
 import line from "../images/pattern-divider-desktop.svg";
 import icon from "../images/icon-dice.svg";
+import { useState, useEffect } from "react";
 
-export const Advice = ({ advice }) => {
+export const Advice = () => {
+  const [advice, setAdvice] = useState(null);
+
+  const fetchAdvice = async () => {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    setAdvice(data.slip);
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
     <div className="flex flex-col">
-      <div className="bg-dark-grayish-blue w-[460px] h-80 text-center rounded-3xl p-10 flex flex-col justify-around">
-        <div className="text-neon-green font-bold tracking-widest text-xs">
-          {`ADVICE #${advice?.id}`}
-        </div>
-        <div className="text-light-cyan font-bold text-2xl">
-          {`"${advice?.advice}"`}
-        </div>
-        <img src={line} alt="line" className="w-full" />
-      </div>
-      <button
-        className="rounded-full h-16 w-16 bg-neon-green flex justify-center items-center relative bottom-8 left-[198px] hover:shadow-neon-green hover:shadow-neon-green/80 transition-shadow duration-300"
-        onClick={() => {
-          console.log("clic");
-        }}
-      >
-        <img src={icon} alt="icon" className="w-6" />
-      </button>
+      {advice && (
+        <>
+          <div className="bg-dark-grayish-blue w-[460px] h-80 text-center rounded-3xl p-10 flex flex-col justify-around">
+            <div className="text-neon-green font-bold tracking-widest text-xs">
+              {`ADVICE #${advice?.id}`}
+            </div>
+            <div className="text-light-cyan font-bold text-2xl">
+              {`"${advice?.advice}"`}
+            </div>
+            <img src={line} alt="line" className="w-full" />
+          </div>
+          <button
+            className="rounded-full h-16 w-16 bg-neon-green flex justify-center items-center relative bottom-8 left-[198px] hover:shadow-neon-green hover:shadow-neon-green/80 transition-shadow duration-300"
+            onClick={fetchAdvice}
+          >
+            <img src={icon} alt="icon" className="w-6" />
+          </button>
+        </>
+      )}
     </div>
   );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Advice } from "./Advice/Advice";
-import { useFetch } from "./useFetch";
+// import { useFetch } from "./useFetch";
 
 function App() {
-  const data = useFetch("https://api.adviceslip.com/advice");
+  // const data = useFetch("https://api.adviceslip.com/advice");
 
   return (
     <div className="h-screen bg-dark-blue flex justify-center items-center font-manrope">
-      {data && <Advice advice={data.slip} />}
+      <Advice />
     </div>
   );
 }
