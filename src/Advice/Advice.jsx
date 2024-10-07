af83ecd6df63c90c4110d94bc63451d910b554b4
Mariano Tourné <marianotourne@gmail.com>
2024-10-07 18:29:20 -0300

    solution is ok

--- src/Advice/Advice.jsx
+++ src/Advice/Advice.jsx
@@ -16,10 +16,10 @@ export const Advice = () => {
   }, []);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center">
       {advice && (
         <>
-          <div className="bg-dark-grayish-blue w-[460px] h-80 text-center rounded-3xl p-10 flex flex-col justify-around">
+          <div className="bg-dark-grayish-blue w-11/12 h-80 text-center rounded-3xl p-10 flex flex-col justify-around md:w-[460px]">
             <div className="text-neon-green font-bold tracking-widest text-xs">
               {`ADVICE #${advice?.id}`}
             </div>
@@ -29,7 +29,7 @@ export const Advice = () => {
             <img src={line} alt="line" className="w-full" />
           </div>
           <button
-            className="rounded-full h-16 w-16 bg-neon-green flex justify-center items-center relative bottom-8 left-[198px] hover:shadow-neon-green hover:shadow-neon-green/80 transition-shadow duration-300"
+            className="rounded-full h-16 w-16 bg-neon-green flex justify-center items-center relative bottom-8 hover:shadow-neon-green hover:shadow-neon-green/80 transition-shadow duration-300"
             onClick={fetchAdvice}
           >
             <img src={icon} alt="icon" className="w-6" />
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Advice } from "./Advice/Advice";
-// import { useFetch } from "./useFetch";
 
 function App() {
-  // const data = useFetch("https://api.adviceslip.com/advice");
-
   return (
     <div className="h-screen bg-dark-blue flex justify-center items-center font-manrope">
       <Advice />
